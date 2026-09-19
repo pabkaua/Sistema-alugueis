9f61651a8f2591e53b59777aedfb44328c2986a8
Kauã Pablo <kauapablo940@gmail.com>
2026-09-19 13:04:42 -0300

    fix: ajusta exceptions de itembusiness conforme sonar

--- src/main/java/com/loja/business/ItemBusiness.java
+++ src/main/java/com/loja/business/ItemBusiness.java
@@ -4,6 +4,7 @@ import com.loja.business.interfaces.IItemBusiness;
 import com.loja.model.Categoria;
 import com.loja.model.Fornecedor;
 import com.loja.model.Item;
+import com.loja.repositories.PersistenciaException;
 import com.loja.repositories.interfaces.ICategoriaRepository;
 import com.loja.repositories.interfaces.IFornecedorRepository;
 import com.loja.repositories.interfaces.IItemRepository;
@@ -23,13 +24,13 @@ public class ItemBusiness implements IItemBusiness {
 
     public void cadastrar(Item i){
         if (repo.buscar(i.getId()) != null){
-            throw new RuntimeException("Item já cadastrado: " + i.getNome());
+            throw new BusinessException("Item já cadastrado: " + i.getNome());
         }
         else if (catRepo.buscar(i.getCategoria().getId()) == null) {
-            throw new RuntimeException("Categoria não cadastrada: " + i.getCategoria().getNome());
+            throw new BusinessException("Categoria não cadastrada: " + i.getCategoria().getNome());
         }
         else if (fornRepo.buscar(i.getFornecedor().getId()) == null){
-            throw new RuntimeException("Fornecedor não cadastrado: " + i.getFornecedor().getNome());
+            throw new BusinessException("Fornecedor não cadastrado: " + i.getFornecedor().getNome());
         }
 
         if (i.getStatus().isBlank() || i.getStatus() == null){
@@ -41,7 +42,7 @@ public class ItemBusiness implements IItemBusiness {
     public Item buscar(String id){
         Item item = repo.buscar(id);
         if(item == null){
-            throw new RuntimeException("Item não encontrado: " + id);
+            throw new BusinessException("Item não encontrado: " + id);
         }
         return item;
     }
@@ -56,46 +57,50 @@ public class ItemBusiness implements IItemBusiness {
 
     public Map<String, Item> listarPorCategoria(Categoria categoria){
         if (categoria == null){
-            throw new RuntimeException("Categoria inválida!");
+            throw new BusinessException("Categoria inválida!");
         } else if (categoria.getNome() == null || categoria.getNome().isBlank()) {
-            throw new RuntimeException("Nome da categoria inválida!");
+            throw new BusinessException("Nome da categoria inválida!");
         } else if (catRepo.buscar(categoria.getId()) == null){
-            throw new RuntimeException("Categoria não cadastrada: " + categoria.getNome());
+            throw new BusinessException("Categoria não cadastrada: " + categoria.getNome());
         }
         return repo.listar(categoria);
     }
 
     public Map<String, Item> listarPorFornecedor(Fornecedor fornecedor){
         if (fornecedor == null){
-            throw new RuntimeException("Fornecedor inválido!");
+            throw new BusinessException("Fornecedor inválido!");
         } else if (fornecedor.getNome() == null || fornecedor.getNome().isBlank()){
-            throw new RuntimeException("Nome do fornecedor inválido!");
+            throw new BusinessException("Nome do fornecedor inválido!");
         } else if (fornRepo.buscar(fornecedor.getId()) == null){
-            throw new RuntimeException("Fornecedor não cadastrado: " + fornecedor.getNome());
+            throw new BusinessException("Fornecedor não cadastrado: " + fornecedor.getNome());
         }
         return repo.listar(fornecedor);
     }
 
     public void atualizar(Item item){
         if (item == null){
-            throw new RuntimeException("Item inválido!");
+            throw new BusinessException("Item inválido!");
         }
         if (!repo.atualizar(item)){
-            throw new RuntimeException("Não foi possível atualizar!");
+            throw new BusinessException("Não foi possível atualizar!");
         }
     }
 
     public void deletar(String id){
         if (repo.buscar(id) == null){
-            throw new RuntimeException("Item não encontrado: " + id);
+            throw new BusinessException("Item não encontrado: " + id);
         }
         if (repo.buscar(id).hasHistorico()){
-            throw new RuntimeException("O item não pode ser excluido, tem histórico");
+            throw new BusinessException("O item não pode ser excluido, tem histórico");
         }
         repo.deletar(id);
     }
 
     public void salvarDados(){
-        this.repo.salvarDados();
+        try{
+            this.repo.salvarDados();
+        } catch (PersistenciaException e){
+            throw new BusinessException("Erro ao salvar dados" + e.getMessage());
+        }
     }
 }
